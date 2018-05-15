96fcadaaddb72f7e6b6453f7023c6682ee661b98
zhouyiqi <137968399@qq.com>
2018-05-15 10:11:55 +0800

    fixbug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'VicBBS') - Laravel 进阶教程</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
