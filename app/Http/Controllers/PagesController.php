9946c9b6816947f27530e4f12520ec2c43464206
zhouyiqi <137968399@qq.com>
2018-05-16 17:53:10 +0800

    fixbug

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,8 +11,44 @@ class PagesController extends Controller
         return view('pages.root');
     }
 
-    public function test()
+    /**
+     * @SWG\Get(
+     *     path="/api/test",
+     *     description="返回测试内容",
+     *     operationId="api.dashboard.index",
+     *     produces={"application/json"},
+     *     tags={"测试"},
+     *     @SWG\Parameter(
+     *         in="formData",
+     *         name="reason",
+     *         type="string",
+     *         description="拿数据的理由",
+     *         required=true,
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Dashboard overview."
+     *     ),
+     *     @SWG\Response(
+     *         response=401,
+     *         description="Unauthorized action.",
+     *     )
+     * )
+     */
+    public function test(Request $request)
     {
-        return 1111;
+        return response()->json([
+            'result' => [
+                'statistics' => [
+                    'users' => [
+                        'name' => 'Name',
+                        'email' => 'user@example.com'
+                    ]
+                ],
+            ],
+            'message' => '',
+            'type' => 'success',
+            'status' => 0
+        ]);
     }
 }
