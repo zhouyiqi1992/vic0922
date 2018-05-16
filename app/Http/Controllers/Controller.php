9602e58fcea7310c8bea0e0386153ced206467ca
zhouyiqi <137968399@qq.com>
2018-05-16 17:45:57 +0800

    fixbug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,18 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
+/**
+ * @SWG\Swagger(
+ *   @SWG\Info(
+ *     title="Site Title",
+ *     version="1.0",
+ *     description="Site description",
+ *     @SWG\Contact(
+ *         email="xyz@xyz.com"
+ *     )
+ *   )
+ * )
+ */
 
 class Controller extends BaseController
 {
