545d9e14e70b017dac4db55d691448d20a75e9a1
zhouyiqi <137968399@qq.com>
2018-05-16 18:16:31 +0800

    fixbug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,35 +6,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-/**
- * Class Controller
- *
- * @package App\Http\Controllers
- *
- * @SWG\Swagger(
- *     basePath="",
- *     host="127.0.0.1",
- *     schemes={"http"},
- *     @SWG\Info(
- *         version="2.0",
- *         title="OpenApi",
- *         @SWG\Contact(name="Pek Ratanak", url="https://www.google.com"),
- *     ),
- *     @SWG\Definition(
- *         definition="Error",
- *         required={"code", "message"},
- *         @SWG\Property(
- *             property="code",
- *             type="integer",
- *             format="int32"
- *         ),
- *         @SWG\Property(
- *             property="message",
- *             type="string"
- *         )
- *     )
- * )
- */
+
 
 class Controller extends BaseController
 {
