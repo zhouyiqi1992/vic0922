16f28c85e720ef5ba4dbe24460501d790dc45921
zhouyiqi <137968399@qq.com>
2018-05-16 14:34:15 +0800

    fixbug

--- app/Http/Controllers/SwaggerController.php
+++ app/Http/Controllers/SwaggerController.php
@@ -1,36 +1,79 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Swagger\Annotations\Info;
+use Illuminate\Http\Request;
 use Swagger\Annotations as SWG;
+/**
+ * @SWG\Swagger(
+ *     schemes={"http","https"},
+ *     host="api.host.com",
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="This is my website cool API",
+ *         description="Api description...",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="contact@mysite.com"
+ *         ),
+ *         @SWG\License(
+ *             name="Private License",
+ *             url="URL to the license"
+ *         )
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Find out more about my website",
+ *         url="http..."
+ *     ),
+ *      // define tag
+ *     @SWG\Tag(name="Home", description="Roote Route"),
+ *     @SWG\Tag(name="User", description="UserController"),
+ *     @SWG\Tag(name="Role", description="RoleController"),
+ *     @SWG\ExternalDocumentation(
+ *         description="Find out more about my website",
+ *         url="http..."
+ *     ),
+ *    @SWG\Definition(
+ *       definition="errorModel",
+ *       required={"status code", "message"},
+ *       @SWG\Property(
+ *           property="status code",
+ *           type="integer",
+ *           format="int32"
+ *       ),
+ *       @SWG\Property(
+ *           property="message",
+ *           type="string"
+ *       )
+ *   ),
+ *    // 定义 API
+ *   @SWG\Definition(
+ *     definition="Login",
+ *      @SWG\Property(
+ *        property="useraccount",
+ *        type="string"
+ *      ),
+ *      @SWG\Property(
+ *         property="password",
+ *         type="string"
+ *      )
+ *    ),
+ *    @SWG\Definition(
+ *       definition="logout",
+ *       @SWG\Property(
+ *           property="token",
+ *           type="string"
+ *       )
+ *   ),
+ * )
+ */
+
 
 class SwaggerController extends Controller
 {
-    /**
-     * @Swagger(
-     *     schemes={"http"},
-     *     basePath="/",
-     *     consumes={"application/json"},
-     *     tags={
-     *         @SWG\Tag(
-     *             name="API",
-     *             description="API接口"
-     *         )
-     *     }
-     * )
-     *
-     * @Info(
-     *  title="API文档",
-     *  version="0.1"
-     * )
-     *
-     * @return mixed
-     */
-
     public function doc()
     {
-        $swagger = \Swagger\scan(realpath(__DIR__.'/../../'));
+        $swagger = \Swagger\scan(realpath(__DIR__ . '/../../'));
         return response()->json($swagger);
     }
 }
