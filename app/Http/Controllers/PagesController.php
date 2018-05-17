c17f847cd46b6c028b466317a8fd731e4f657519
zhouyiqi <137968399@qq.com>
2018-05-17 10:46:43 +0800

    fixbug

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,49 +6,12 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     *
+     */
     public function root()
     {
         return view('pages.root');
-    }
-
-    /**
-     * @SWG\Get(
-     *     path="/api/test",
-     *     description="返回测试内容",
-     *     operationId="api.dashboard.index",
-     *     produces={"application/json"},
-     *     tags={"测试"},
-     *     @SWG\Parameter(
-     *         in="formData",
-     *         name="reason",
-     *         type="string",
-     *         description="拿数据的理由",
-     *         required=true,
-     *     ),
-     *     @SWG\Response(
-     *         response=200,
-     *         description="Dashboard overview."
-     *     ),
-     *     @SWG\Response(
-     *         response=401,
-     *         description="Unauthorized action.",
-     *     )
-     * )
-     */
-    public function test(Request $request)
-    {
-        return response()->json([
-            'result' => [
-                'statistics' => [
-                    'users' => [
-                        'name' => 'Name',
-                        'email' => 'user@example.com'
-                    ]
-                ],
-            ],
-            'message' => '',
-            'type' => 'success',
-            'status' => 0
-        ]);
     }
 }
