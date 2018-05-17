dec4d89556135dda73cdc063bfaa9d6bf36d82b3
zhouyiqi <137968399@qq.com>
2018-05-17 15:02:14 +0800

    fixbug

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -14,4 +14,32 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+    /**
+     * @SWG\Get(
+     *     path = "/testSwg/{user_id}",
+     *     description = "这里是文档描述",
+     *     summary = "这里是接口简介",
+     *     tags = "这是接口标签，可以有多个",
+     *     operationId = "getMyData",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *     name="user_id",
+     *     in = "query",
+     *     type = "integer",
+     *     required="true",
+     *     description="用户id",
+     *     )
+     * )
+     *
+     * @SWG\Response(
+     *     response = 200,
+     *     description = "响应结果的描述"
+     * )
+     *
+     */
+    public function testSwg()
+    {
+
+    }
 }
