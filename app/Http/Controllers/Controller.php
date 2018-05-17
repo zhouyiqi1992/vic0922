b8fb4ce1cbd9f796c66cb73d800061baf63d9092
zhouyiqi <137968399@qq.com>
2018-05-17 10:49:28 +0800

    fixbug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,5 +10,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
+    /**
+     * 返回JSON格式的Swagger定义
+     *
+     * 这里需要一个主`Swagger`定义：
+     * @SWG\Swagger(
+     *   @SWG\Info(
+     *     title="我的`Swagger`API文档",
+     *     version="1.0.0"
+     *   )
+     * )
+     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
