418ec4226b4e072962b0607e9cf0effc3e87ab1e
zhouyiqi <137968399@qq.com>
2018-05-21 10:02:07 +0800

    fixbug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Swagger\Annotations\Schema;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
